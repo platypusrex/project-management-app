5e9a4e9148884b7c29a0a054e716516e417cba4d
frankcooke <frankcooke79@gmail.com>
2018-06-12 19:37:31 -0400

    Rework - continues adding functionality for the select component

--- client/src/shared/components/Select.js
+++ client/src/shared/components/Select.js
@@ -1,59 +1,109 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { compose, withHandlers } from 'recompose';
-import { withState } from "../containers/withState";
+import Icon from 'react-ionicons';
 import '../../styles/components/Select.css';
 
-const initialState = {
-	isOptionsMenuOpen: false,
+export class Select extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.selectRef = React.createRef();
+
+    this.state = {
+      isOptionsMenuOpen: false,
+      selectedOption: null
+    };
+  }
+
+  componentDidMount () {
+    document.addEventListener('click', this.handleClickOutside, false);
+  }
+
+  componentWillUnmount () {
+    document.removeEventListener('click', this.handleClickOutside, false);
+  }
+
+  handleClickOutside = (e) => {
+    if (!this.selectRef) {
+      return;
+    }
+
+    if (!this.selectRef.current.contains(e.target)) {
+      this.setState(ss => ({...ss, isOptionsMenuOpen: false}));
+    }
+  };
+
+  handleOptionClick = (option) => {
+    const { selectedOption } = this.state;
+
+    if (Object.is(option, selectedOption)) {
+      return;
+    }
+
+    this.setState(ss => ({...ss, selectedOption: option, isOptionsMenuOpen: false}))
+  };
+
+  getOptionClassName = (option) => {
+    const { optionPrefixCls } = this.props;
+    const { selectedOption } = this.state;
+
+    return Object.is(option, selectedOption) ? `${optionPrefixCls} ${optionPrefixCls}--selected` : optionPrefixCls;
+  };
+
+  render () {
+    const { options, placeholder, selectTextPrefixCls, optionsWrapperPrefixCls } = this.props;
+    const { isOptionsMenuOpen, selectedOption } = this.state;
+    const optionsWrapperClass = isOptionsMenuOpen ?
+      `${optionsWrapperPrefixCls} ${optionsWrapperPrefixCls}--open` :
+      optionsWrapperPrefixCls;
+    const selectTextClass = isOptionsMenuOpen ?
+      `${selectTextPrefixCls} ${selectTextPrefixCls}--open` :
+      selectTextPrefixCls;
+    const placeHolder = (selectedOption && selectedOption.label) || (placeholder || 'Choose one...');
+
+    return (
+      <div className="select" ref={this.selectRef}>
+        <div
+          className={selectTextClass}
+          onClick={() => this.setState(ss => ({...ss, isOptionsMenuOpen: !ss.isOptionsMenuOpen}))}
+        >
+          <span className="select__text">{placeHolder}</span>
+          <div className="select__icon-wrapper">
+            <Icon icon="ios-arrow-down" fontSize="18px"/>
+          </div>
+        </div>
+        <div className={optionsWrapperClass}>
+          {options.map((option, i) => (
+            <div
+              key={i}
+              className={this.getOptionClassName(option)}
+              onClick={() => this.handleOptionClick(option)}
+            >
+              <span className="select__option__text">{option.label}</span>
+            </div>
+          ))}
+        </div>
+        <select className="select__input">
+          {options.map((option, i) => <option key={i} value={option.value}>{option.label}</option>)}
+        </select>
+      </div>
+    )
+  }
+}
+
+Select.defaultProps = {
+  selectTextPrefixCls: 'select__text-wrapper',
+	optionsWrapperPrefixCls: 'select__options-wrapper',
+  optionPrefixCls: 'select__option'
 };
 
-const SelectComponent = (props) => {
-	const { options, optionsPrefixCls, setState, state } = props;
-	const optionWrapperClass = state.isOptionsMenuOpen ?
-		`${optionsPrefixCls} ${optionsPrefixCls}--open` :
-		optionsPrefixCls;
-
-	return (
-		<div className="select">
-			<span
-				className="select__text"
-				onClick={() => setState(ss => ({...ss, isOptionsMenuOpen: !ss.isOptionsMenuOpen}))}
-			>
-				Choose one...
-			</span>
-			<div className={optionWrapperClass}>
-				{options.map((option, i) => (
-					<div key={i} className="select__option">
-						<span className="select__option__text">{option.label}</span>
-					</div>
-				))}
-			</div>
-			<select className="select__input">
-				{options.map((option, i) => <option key={i} value={option.value}>{option.label}</option>)}
-			</select>
-		</div>
-	);
-};
-
-SelectComponent.defaultProps = {
-	optionsPrefixCls: 'select__options-wrapper'
-};
-
-SelectComponent.propTypes = {
+Select.propTypes = {
 	options: PropTypes.arrayOf(
 		PropTypes.shape({
 			value: PropTypes.string,
 			label: PropTypes.string
 		})
-	)
+	),
+  defaultValue: PropTypes.string,
+  placeholder: PropTypes.string
 };
-
-export const Select = compose(
-	withState(initialState),
-	withHandlers({
-		handleSelectClick: (props) => () => {
-
-		}
-	})
-)(SelectComponent);
