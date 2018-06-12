c7f9c53cb57fd3ed56229030cd01a4ba9a9b1265
platypusrex <frankcooke79@gmail.com>
2018-06-12 07:54:12 -0400

    Rework - continue working on custom select component

--- client/src/shared/components/Select.js
+++ client/src/shared/components/Select.js
@@ -1,13 +1,59 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { compose } from 'recompose';
+import { compose, withHandlers } from 'recompose';
+import { withState } from "../containers/withState";
+import '../../styles/components/Select.css';
+
+const initialState = {
+	isOptionsMenuOpen: false,
+};
 
 const SelectComponent = (props) => {
-	return (
+	const { options, optionsPrefixCls, setState, state } = props;
+	const optionWrapperClass = state.isOptionsMenuOpen ?
+		`${optionsPrefixCls} ${optionsPrefixCls}--open` :
+		optionsPrefixCls;
 
+	return (
+		<div className="select">
+			<span
+				className="select__text"
+				onClick={() => setState(ss => ({...ss, isOptionsMenuOpen: !ss.isOptionsMenuOpen}))}
+			>
+				Choose one...
+			</span>
+			<div className={optionWrapperClass}>
+				{options.map((option, i) => (
+					<div key={i} className="select__option">
+						<span className="select__option__text">{option.label}</span>
+					</div>
+				))}
+			</div>
+			<select className="select__input">
+				{options.map((option, i) => <option key={i} value={option.value}>{option.label}</option>)}
+			</select>
+		</div>
 	);
 };
 
-export const Select= compose(
+SelectComponent.defaultProps = {
+	optionsPrefixCls: 'select__options-wrapper'
+};
 
+SelectComponent.propTypes = {
+	options: PropTypes.arrayOf(
+		PropTypes.shape({
+			value: PropTypes.string,
+			label: PropTypes.string
+		})
+	)
+};
+
+export const Select = compose(
+	withState(initialState),
+	withHandlers({
+		handleSelectClick: (props) => () => {
+
+		}
+	})
 )(SelectComponent);
