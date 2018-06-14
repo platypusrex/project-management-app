59a74e51f7450dd914f86c33c27114d52110bb11
frankcooke <frankcooke79@gmail.com>
2018-06-13 22:22:01 -0400

    hmm - not sure what changed here...interesting

--- client/src/routes/Home/components/ProjectListItem.js
+++ client/src/routes/Home/components/ProjectListItem.js
@@ -9,52 +9,52 @@ import Icon from 'react-ionicons';
 import { withDeleteProject } from "../../../api/project/withDeleteProject";
 
 const ProjectListItemComponent = (props) => {
-	const { project } = props;
+  const { project } = props;
 
-	const overlay = (
-		<DropdownMenu
-			menuItems={['edit', 'delete']}
-			onClick={(menuItem) => props.handleDropdownClick(menuItem)}
-		/>
-	);
+  const overlay = (
+    <DropdownMenu
+      menuItems={['edit', 'delete']}
+      onClick={(menuItem) => props.handleDropdownClick(menuItem)}
+    />
+  );
 
-	const dropdown = (
-		<Dropdown overlay={overlay}>
-			<Icon icon="md-more" fontSize="20px" style={{cursor: 'pointer'}}/>
-		</Dropdown>
-	);
+  const dropdown = (
+    <Dropdown overlay={overlay}>
+      <Icon icon="md-more" fontSize="20px" style={{cursor: 'pointer'}}/>
+    </Dropdown>
+  );
 
-	return (
-		<ListItem
-			title={<a onClick={() => props.history.push(`/project/${project.id}`)}>{project.title}</a>}
-			description={project.description}
-			extra={dropdown}
-		/>
-	);
+  return (
+    <ListItem
+      title={<a onClick={() => props.history.push(`/project/${project.id}`)}>{project.title}</a>}
+      description={project.description}
+      extra={dropdown}
+    />
+  );
 };
 
 ProjectListItemComponent.propTypes = {
-	project: PropTypes.object.isRequired,
-	editProject: PropTypes.func.isRequired,
+  project: PropTypes.object.isRequired,
+  editProject: PropTypes.func.isRequired,
 };
 
 export const ProjectListItem = compose(
-	withRouter,
-	withDeleteProject,
-	withHandlers({
-		handleDropdownClick: (props) => async (menuItem) => {
-			const { project, deleteProject, editProject } = props;
-			if (menuItem === 'delete') {
-				try {
-					await deleteProject({projectId: project.id});
-				} catch (err) {
-					console.log(`ProjectListItem: handle delete ${err}`);
-				}
-			}
+  withRouter,
+  withDeleteProject,
+  withHandlers({
+    handleDropdownClick: (props) => async (menuItem) => {
+      const { project, deleteProject, editProject } = props;
+      if (menuItem === 'delete') {
+        try {
+          await deleteProject({projectId: project.id});
+        } catch (err) {
+          console.log(`ProjectListItem: handle delete ${err}`);
+        }
+      }
 
-			if (menuItem === 'edit') {
-				editProject(project);
-			}
-		}
-	})
+      if (menuItem === 'edit') {
+        editProject(project);
+      }
+    }
+  })
 )(ProjectListItemComponent);
