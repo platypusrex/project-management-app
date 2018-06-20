e5cd28479be064f5bde76ef90db21488d92dcc7c
frankcooke <frankcooke79@gmail.com>
2018-06-20 10:24:35 -0400

    Add- withCreateTask hoc

---
+++ client/src/api/task/withCreateTask.js
@@ -0,0 +1,38 @@
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+import {TasksByColumnIdQuery} from "./withTasksByColumnId";
+
+const CreateTaskMutation = gql`
+  mutation ($columnId: Int!, $createdBy: Int!, $task: String!) {
+    createTask (columnId: $columnId, createdBy: $createdBy, task: $task) {
+      id
+      createdAt
+      updatedAt
+      task
+    }
+  }
+`;
+
+export const withCreateTask = graphql(CreateTaskMutation, {
+  props: ({ownProps, mutate}) => ({
+    createTask: async (variables) => {
+      console.log('own props', ownProps);
+      const options = {
+        variables,
+        mutation: CreateTaskMutation,
+        refetchQueries: [{
+          query: TasksByColumnIdQuery,
+          variables: {
+            columnId: ownProps.columnId
+          }
+        }]
+      };
+
+      if (!mutate) {
+        throw new Error('withCreateTask: missing mutate');
+      }
+
+      return mutate(options);
+    }
+  })
+});
