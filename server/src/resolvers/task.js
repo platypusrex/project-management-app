5a49d4264cb7f7eee9c4c9d20296cb3c83317398
frankcooke <frankcooke79@gmail.com>
2018-06-20 09:24:44 -0400

    Rework - ad getTasksByColumnId to task resolvers

--- server/src/resolvers/task.js
+++ server/src/resolvers/task.js
@@ -2,14 +2,20 @@ export const taskResolvers = {
 	Task: {
 		column: (parent, args, {models}) => models.Column.findOne({
 			where: {id: parent.columnId}
-		})
+		}),
+    creator: (parent, args, {models}) => models.User.findOne({
+      where: {id: parent.createdBy}
+    })
 	},
 
 	Query: {
 		getAllTasks: (parent, args, {models}) => models.Task.findAll(),
 		getTaskById: (parent, {taskId}, {models}) => models.Task.findOne({
 			where: {id: taskId}
-		})
+		}),
+    getTasksByColumnId: (parent, {columnId}, {models}) => models.Task.findAll({
+      where: {columnId}
+    })
 	},
 
 	Mutation: {
