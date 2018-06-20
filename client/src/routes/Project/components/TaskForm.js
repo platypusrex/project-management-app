0a257731e5b3106c736ef4ddec8e7c368bcd35b3
frankcooke <frankcooke79@gmail.com>
2018-06-20 10:06:58 -0400

    Add - Tooltip component

---
+++ client/src/routes/Project/components/TaskForm.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { compose, withHandlers } from 'recompose';
+import {Button} from "../../../shared/components/Button";
+import { Modal } from "../../../shared/components/Modal";
+import { FormGroup } from "../../../shared/components/FormGroup";
+import { Input } from "../../../shared/components/Input";
+import { withState } from "../../../shared/containers/withState";
+import {withCreateTask} from "../../../api/task/withCreateTask";
+import {withUserId} from "../../../shared/utils/localStorageUtil";
+
+const initialState = {
+  isSubmitting: false,
+  task: ''
+};
+
+const TaskFormComponent = (props) => {
+  const { dismiss, setState, state } = props;
+  const { task } = state;
+
+  const footer = (
+    <Button style={{marginLeft: 'auto'}} onClick={props.handleSubmit}>
+      Submit
+    </Button>
+  );
+
+  return (
+    <Modal
+      title="Add Task"
+      dismiss={dismiss}
+      footer={footer}
+      width={450}
+    >
+      <FormGroup label="Task">
+        <Input
+          type="text"
+          value={task}
+          onChange={task => setState(ss => ({...ss, task}))}
+        />
+      </FormGroup>
+    </Modal>
+  );
+};
+
+TaskFormComponent.propTypes = {
+  columnId: PropTypes.number.isRequired,
+  dismiss: PropTypes.func.isRequired
+};
+
+export const TaskForm = compose(
+  withState(initialState),
+  withUserId,
+  withCreateTask,
+  withHandlers({
+    handleSubmit: (props) => async (e) => {
+      e.preventDefault();
+      const { columnId, userId, state, setState } = props;
+      const { task, isSubmitting, errors } = state;
+
+      if (isSubmitting) {
+        return;
+      }
+
+      try {
+        setState(ss => ({...ss, isSubmitting: true}));
+        await props.createTask({task, columnId, createdBy: userId});
+        setState(ss => ({...ss, isSubmitting: false}));
+        props.dismiss();
+      } catch (err) {
+        setState(ss => ({...ss, isSubmitting: false}));
+        console.log(`handleCreateColumn: ${err}`);
+      }
+    }
+  })
+)(TaskFormComponent);
---
+++ client/src/shared/components/Tooltip.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { compose, lifecycle, withHandlers } from 'recompose';
+import { withState } from "../containers/withState";
+import '../../styles/components/Tooltip.css';
+
+const initialState = {
+  isTooltipVisible: false,
+  tooltipStyle: {}
+};
+
+const positions = {
+  left: 'left',
+  right: 'right',
+};
+
+const TooltipComponent = (props) => {
+  const { children, title, state } = props;
+  const tooltipClass = getTooltipWrapperClass(props);
+
+  return (
+    <div
+      className="tooltip"
+      onMouseEnter={props.handleMouseEnter}
+      onMouseLeave={props.handleMouseLeave}
+    >
+      {children}
+
+      <div className={tooltipClass} style={state.tooltipStyle}>
+        <span className="tooltip__title">{title}</span>
+      </div>
+    </div>
+  );
+};
+
+TooltipComponent.defaultProps = {
+  toolTipPrefixCls: 'tooltip__wrapper'
+};
+
+TooltipComponent.propTypes = {
+  title: PropTypes.string,
+  children: PropTypes.node,
+  position: PropTypes.oneOf(Object.keys(positions).map(key => positions[key]))
+};
+
+function getTooltipWrapperClass (props) {
+  const { position, toolTipPrefixCls, state } = props;
+  const toolTipClass = `${toolTipPrefixCls} ${getTooltipPositionClass(position, toolTipPrefixCls)}`;
+
+  return state.isTooltipVisible ? `${toolTipPrefixCls}--visible ${toolTipClass}` : toolTipClass;
+}
+
+function getTooltipPositionClass (position, prefix) {
+  switch (position) {
+    case positions.left:
+      return `${prefix}--${positions.left}`;
+    case positions.right:
+      return `${prefix}--${positions.right}`;
+    default:
+      return `${prefix}--${positions.left}`;
+  }
+}
+
+export const Tooltip = compose(
+  withState(initialState),
+  lifecycle({
+    componentDidMount: function () {
+      const { setState } = this.props;
+      const tooltip = document.querySelector('.tooltip__wrapper');
+
+      setState(ss => ({
+        ...ss,
+        tooltipStyle: {marginBottom: `-${tooltip.clientHeight / 2}px`}
+      }));
+    }
+  }),
+  withHandlers({
+    handleMouseEnter: (props) => () => {
+      const { setState } = props;
+
+      setState(ss => ({...ss, isTooltipVisible: true}))
+    },
+    handleMouseLeave: (props) => () => {
+      const { setState } = props;
+
+      setState(ss => ({...ss, isTooltipVisible: false}))
+    }
+  })
+)(TooltipComponent);
