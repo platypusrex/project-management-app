fa54d114a6d6e3e9c8ddd60414fa5597ef6afa5a
frankcooke <frankcooke79@gmail.com>
2018-06-21 10:37:09 -0400

    Rework - add handler task drop on column target

--- client/src/routes/Project/components/ColumnTasks.js
+++ client/src/routes/Project/components/ColumnTasks.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { compose, withProps, branch, renderComponent } from 'recompose';
+import { compose, withProps, branch, renderComponent, withHandlers } from 'recompose';
 import { Task } from "./Task";
 import { withTasksByColumnId } from "../../../api/task/withTasksByColumnId";
+import { withUpdateTaskById } from "../../../api/task/withUpdateTaskById";
 
 const ColumnTasksComponent = (props) => {
   const { tasks } = props;
@@ -11,7 +12,7 @@ const ColumnTasksComponent = (props) => {
     <div className="column-tasks grid">
       {tasks.map(task => (
         <div key={task.id} className="col-12">
-          <Task task={task} handleDrop={id => console.log('id', id)}/>
+          <Task task={task} handleDrop={(task, newColumnId) => props.handleDrop(task, newColumnId)}/>
         </div>
       ))}
     </div>
@@ -24,6 +25,7 @@ ColumnTasksComponent.propTypes = {
 
 export const ColumnTasks = compose(
   withTasksByColumnId,
+  withUpdateTaskById,
   withProps(props => {
     const { tasksData } = props;
     const tasks =
@@ -33,6 +35,19 @@ export const ColumnTasks = compose(
     return {
       tasks
     };
+  }),
+  withHandlers({
+    handleDrop: (props) => async (task, newColumnId) => {
+      if (newColumnId === task.columnId) {
+        return;
+      }
+
+      try {
+        await props.updateTaskById({columnId: newColumnId, taskId: task.id});
+      } catch (err) {
+        console.log('err', err);
+      }
+    }
   }),
   branch(props => props.tasksData.loading,
     renderComponent(() => <div>Loading...</div>)
