f5b0a64359476c90235dea3ade51f2cbfc1a933c
frankcooke <frankcooke79@gmail.com>
2018-06-21 23:41:11 -0400

    Rework - add a back button to the PageHeader component

--- client/src/shared/components/PageHeader.js
+++ client/src/shared/components/PageHeader.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+import { compose } from 'recompose';
+import { Icon } from "./Icon";
 import '../../styles/components/PageHeader.css';
 
-export const PageHeader = (props) => {
+export const PageHeaderComponent = (props) => {
   const { title, subTitle, children } = props;
   return (
     <div className="page-header">
-      <div className="page-header__title-wrapper">
-        <h3 className="page-header__title">{title}</h3>
-        <span className="page-header__sub-title">{subTitle}</span>
+      <div className="page-header__left">
+        <div className="page-header__back-btn-wrapper">
+          <Icon
+            icon="md-arrow-back"
+            fontSize="24px"
+            style={{cursor: 'pointer'}}
+            onClick={() => props.history.goBack()}
+          />
+        </div>
+
+        <div className="page-header__title-wrapper">
+          <h3 className="page-header__title">{title}</h3>
+          <span className="page-header__sub-title">{subTitle}</span>
+        </div>
       </div>
 
       {children &&
@@ -19,8 +33,12 @@ export const PageHeader = (props) => {
   );
 };
 
-PageHeader.propTypes = {
+PageHeaderComponent.propTypes = {
   title: PropTypes.node.isRequired,
   subTitle: PropTypes.node,
   children: PropTypes.node
 };
+
+export const PageHeader = compose(
+  withRouter
+)(PageHeaderComponent);
