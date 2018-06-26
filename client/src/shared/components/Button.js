624b22a3b06c3f4a7826e5f4dd776fe147b207ea
frankcooke <frankcooke79@gmail.com>
2018-06-26 14:56:55 -0400

    Rework - adds button types and functionality for adding the modifier class to button wrapper

--- client/src/shared/components/Button.js
+++ client/src/shared/components/Button.js
@@ -2,12 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import '../../styles/components/Button.css';
 
+const buttonTypes = {
+  default: 'default',
+  primary: 'primary',
+  danger: 'danger'
+};
+
 export const Button = (props) => {
 	const { children, onClick, disabled, style } = props;
+	const buttonClass = getButtonClass(props);
 
 	return (
 		<button
-			className="button"
+			className={buttonClass}
 			disabled={disabled}
 			onClick={onClick}
 			style={style}
@@ -17,9 +24,29 @@ export const Button = (props) => {
 	);
 };
 
+Button.defaultProps = {
+  buttonPrefixCls: 'button'
+};
+
 Button.propTypes = {
 	children: PropTypes.node,
 	onClick: PropTypes.func,
 	disabled: PropTypes.bool,
 	style: PropTypes.object,
+  type: PropTypes.oneOf(Object.keys(buttonTypes).map(key => buttonTypes[key]))
 };
+
+function getButtonClass (props) {
+  const { type, buttonPrefixCls } = props;
+
+  switch (type) {
+    case buttonTypes.default:
+      return `${buttonPrefixCls} ${buttonPrefixCls}--default`;
+    case buttonTypes.primary:
+      return `${buttonPrefixCls} ${buttonPrefixCls}--primary`;
+    case buttonTypes.danger:
+      return `${buttonPrefixCls} ${buttonPrefixCls}--danger`;
+    default:
+      return `${buttonPrefixCls} ${buttonPrefixCls}--default`;
+  }
+}
