31477b9ebc0a5242ddf44d1a01ea9cb350cddaa9
frankcooke <frankcooke79@gmail.com>
2018-06-26 15:04:54 -0400

    Rework - pass team to the TeamProfile component in Team.js

--- client/src/routes/Team/Team.js
+++ client/src/routes/Team/Team.js
@@ -4,6 +4,7 @@ import { compose, withProps, branch, renderComponent } from 'recompose';
 import { PageHeader } from "../../shared/components/PageHeader";
 import { Tabs } from "../../shared/components/Tabs";
 import { Tab } from "../../shared/components/Tab";
+import { TeamProfile } from "./components/TeamProfile";
 import { withTeamById } from "../../api/team/withTeamById";
 import '../../styles/routes/Team.css';
 
@@ -12,11 +13,11 @@ const TeamComponent = (props) => {
 
   return (
     <div className="team">
-      <PageHeader title={team.name} subTitle={`Manage the ${team.name} team here`}/>
+      <PageHeader title={team.name} subTitle={`Manage the ${team.name} team here`} hasBackBtn={true}/>
 
       <Tabs defaultActiveKey="profile" isTabCard={true}>
         <Tab title="Profile" tabKey="profile">
-          I'm in profile
+          <TeamProfile team={team}/>
         </Tab>
         <Tab title="Boards" tabKey="boards">
           I'm in boards
