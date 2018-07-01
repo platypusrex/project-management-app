18c9a906d3cbd55118ea92982cd38497141067a6
frankcooke <frankcooke79@gmail.com>
2018-07-01 16:12:01 -0400

    Rework - add createProject function in order to add conditional refetch query

--- client/src/api/project/withCreateProject.js
+++ client/src/api/project/withCreateProject.js
@@ -1,7 +1,9 @@
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
+import { apolloClient } from "../apiGraphqlUtil";
 import { ProjectsByUserIdQuery } from "./withProjectsByUserId";
 import { ProjectSmallFragment } from "../fragments/project/projectSmall";
+import { ProjectsByTeamIdQuery } from "./withProjectsByTeamId";
 
 const CreateProjectMutation = gql`
 	mutation ($title: String!, $description: String, $createdBy: Int!, $teamId: Int) {
@@ -12,18 +14,45 @@ const CreateProjectMutation = gql`
 	${ProjectSmallFragment}
 `;
 
+export async function createProject (variables, hasTeamAssociation) {
+  const options = {
+    variables,
+    mutation: CreateProjectMutation,
+    refetchQueries: () => hasTeamAssociation ?
+      [{
+        query: ProjectsByTeamIdQuery,
+        variables: {teamId: variables.teamId}
+      }]
+      :
+      [{
+        query: ProjectsByUserIdQuery,
+        variables: {userId: variables.createdBy}
+      }]
+  };
+
+  return apolloClient.mutate(options);
+}
+
 export const withCreateProject = graphql(CreateProjectMutation, {
 	props: ({ownProps, mutate}) => ({
 		createProject: (variables) => {
 			const options = {
 				variables,
 				mutation: CreateProjectMutation,
-				refetchQueries: [{
-					query: ProjectsByUserIdQuery,
-					variables: {
-						userId: variables.createdBy
-					}
-				}]
+				refetchQueries: [
+				  {
+            query: ProjectsByUserIdQuery,
+            variables: {
+              userId: variables.createdBy
+            },
+          },
+          {
+            query: ProjectsByTeamIdQuery,
+            variables: {
+              teamId: ownProps.teamId
+            }
+          }
+				]
 			};
 
 			if (!mutate) {
